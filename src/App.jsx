2c5ae5b47136ffa5d3322320643490f9112417e9
tosxin-456 <127943389+tosxin-456@users.noreply.github.com>
2023-08-05 21:48:04 +0100

    started the about us

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/home/Home';
 // import About from './pages/about/About';
 import Navbar from './components/navBar';
-import  About  from './pages/about/About';
+import  About  from './pages/about/about';
 
 function App() {
   return (
     <Router>
       <Navbar/>
       <Switch>
-         <Route  path = '/aboutUs' Component={<About/>}>
+         <Route  path = '/aboutUs'>
+          <About/>
           </Route>
         <Route  path="/">
           <Home />
--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,28 +1,54 @@
 import churchLogo from '../../assets/fa-solid_church.svg'
 import '../../App.css'
-import facebook from '../../assets/Facebook.png'
-import recorder from '../../assets/microphone.svg'
-import aboutUsImg from '../../assets/about us image.png'
 import React, { useState, useEffect } from 'react'
-import youtube from '../../assets/youtube.svg'
-
+import kidsImage from '../../assets/image.png'
+import teacher from '../../assets/teacher.svg'
 
 const About = () => {
   return ( 
     <>
- <footer className='footerNav'>
+    <img src={kidsImage} loading='lazy' className='kidImage' alt="" />
+  <div className='welcomeRemark'>
+    <h1>
+     About us
+    </h1>
+      <p >
+      Welcome to bethel teens club - Where faith meets fun.
+      We are a vibrant and inclusive community specifically designed for teenagers. Whether you're just starting to explore your faith or have been on this journey for a while, we have a place for you here.
+      </p>
+  </div>
+  <div className='aboutUsHeaderAndText'>
+  <h2>Meet us</h2>
+  <div className='aboutUsTextAndImage'>
+    <header>Teachers</header>
+    <p>We as teachers believe in empowering teenagers to take on leadership roles, fostering skills that will serve them well throughout their lives.</p>
+  <img src={teacher} alt="" loading='lazy' />
   <div>
-  <div className='footerNameAndLogo'>
-  <img src="https://res.cloudinary.com/dba1aezsn/image/upload/v1690495972/chapel_logo_cph2jz.png" className='footerLogo' alt="" />
-  <p className='footerName'>The Chapel of Faith</p>
+    <h3>Diana</h3>
+  <p>Diana Samson is a 21year old devoted Christian and worshipper who finds it as a passion to teach and guide youths as well as teenagers according to the alignment of God.</p> 
+  <p>Our teachings are relevant, relatable, and designed to address the real-life challenges and questions that teenagers face in today's world.</p>
+  <p>We believe in empowering teenagers to take on leadership roles, fostering skills that will serve them well throughout their lives.</p>
+ </div>
   </div>
-  <div className='icons'>
-  <a href="https://web.facebook.com/ChapelofFaithUJ/?_rdc=1&_rdr"><img src={facebook} alt="" /></a>
-  <a href="https://the-chapel-of-faith-unijos.mixlr.com/"><img src={recorder} alt="" className='microphone' /></a> 
-  <a href="https://www.youtube.com/@ChapelofFaithUJ"><img src={youtube} alt="" className='youtube' /></a> 
+
   </div>
+  <div className='contactUsHeader'>
+  <h2>Join us</h2>
+ 
   </div>
- </footer>
+   <footer className='footerNav'>
+    <div>
+    <div className='footerNameAndLogo'>
+    <img src="https://res.cloudinary.com/dba1aezsn/image/upload/v1690495972/chapel_logo_cph2jz.png" className='footerLogo' alt="" />
+    <p className='footerName'>The Chapel of Faith</p>
+    </div>
+    <div className='icons'>
+    {/* <a href="https://web.facebook.com/ChapelofFaithUJ/?_rdc=1&_rdr"><img src={facebook} alt="" /></a>
+    <a href="https://the-chapel-of-faith-unijos.mixlr.com/"><img src={recorder} alt="" className='microphone' /></a> 
+    <a href="https://www.youtube.com/@ChapelofFaithUJ"><img src={youtube} alt="" className='youtube' /></a>  */}
+    </div>
+    </div>
+   </footer>
   </>
    );
 }
