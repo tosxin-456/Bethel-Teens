a71ba0d138f70b4f2ad8ca482eb8d0d266a70272
tosxin-456 <127943389+tosxin-456@users.noreply.github.com>
2023-08-05 23:06:03 +0100

    started on emma and a teacher

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/home/Home';
-// import About from './pages/about/About';
 import Navbar from './components/navBar';
-import  About  from './pages/about/about';
+import About from './pages/about/about';
 
 function App() {
   return (
     <Router>
-      <Navbar/>
+      <Navbar />
       <Switch>
-         <Route  path = '/aboutUs'>
-          <About/>
-          </Route>
-        <Route  path="/">
+        <Route path="/aboutUs" component={About} />
+        <Route path="/">
           <Home />
         </Route>
-      
       </Switch>
     </Router>
   );
