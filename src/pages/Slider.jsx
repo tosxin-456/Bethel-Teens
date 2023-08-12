7aee730ec809cea1923da5cb4e92827026eff77f
tosxin-456 <127943389+tosxin-456@users.noreply.github.com>
2023-08-12 07:06:21 +0100

    worked on the gallery page

--- src/pages/Slider.jsx
+++ src/pages/Slider.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React ,{useEffect ,useRef} from 'react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/virtual';
@@ -11,42 +12,64 @@ import picNic2 from '../assets/picNic7.jpg';
 import bakingTeens from '../assets/Bake3.jpg';
 import bake2 from '../assets/bake4.jpg';
 import picNic3 from '../assets/picnicTeens7.jpg';
-import bake3 from '../assets/picNic9.jpg';
+import bake3 from '../assets/picNic9.jpg'
 import '../App.css';
 
+
 const Slider = ({ selectedImage }) => {
+  const swiperRef = useRef(null);
+
+  useEffect(() => {
+    if (swiperRef.current && selectedImage) {
+      const swiperInstance = swiperRef.current.swiper;
+      const slideIndex = swiperInstance.slides.findIndex(
+        (slide) => slide.querySelector('img').src === selectedImage
+      );
+      if (slideIndex !== -1) {
+        swiperInstance.slideTo(slideIndex);
+      }
+    }
+  }, [selectedImage]);
   return (
-    <Swiper
+    <>
+        <Swiper
+      ref={swiperRef}
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
       spaceBetween={10}
-      slidesPerView={3}
+      slidesPerView={1}
       navigation
       pagination={{ clickable: true, dynamicBullets: true }}
       scrollbar={{ draggable: true }}
     >
-      {selectedImage && (
-        <SwiperSlide key={selectedImage}>
-          <img src={selectedImage} alt='' className='img' />
+    
+      <div className='galleryImg' >
+        {selectedImage && (
+          <SwiperSlide>
+            <img src={selectedImage} alt='' className='img' />
+          </SwiperSlide>
+        )}
+        <SwiperSlide>
+          <img src={ bake3} className='img' alt='' />
         </SwiperSlide>
-      )}
-      <SwiperSlide key={bake3}>
-        <img src={bake3} alt='' className='img' />
-      </SwiperSlide>
-      <SwiperSlide key={picNic2}>
-        <img src={picNic2} alt='' className='img' />
-      </SwiperSlide>
-      <SwiperSlide key={bakingTeens}>
-        <img src={bakingTeens} alt='' className='img' />
-      </SwiperSlide>
-      <SwiperSlide key={picNic1}>
-        <img src={picNic1} alt='' className='img' />
-      </SwiperSlide>
-      <SwiperSlide key={bake2}>
-        <img src={bake2} alt='' className='img' />
-      </SwiperSlide>
-      <SwiperSlide key={picNic3}>
-        <img src={picNic3} alt='' className='img' />
-      </SwiperSlide>
+        <SwiperSlide>
+          <img src={ picNic2} className='img' alt='' />
+        </SwiperSlide>
+        <div className='galleryBackground'></div>
+        <SwiperSlide>
+          <img src={ bakingTeens  } alt='' className='img' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={ picNic1 } alt='' className='img' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={ bake2 } alt='' className='img' />
+        </SwiperSlide>
+        <SwiperSlide >
+          <img src={ picNic3  } alt='' className='img' />
+        </SwiperSlide>
+      </div>
     </Swiper>
+    </>
   );
 };
 
